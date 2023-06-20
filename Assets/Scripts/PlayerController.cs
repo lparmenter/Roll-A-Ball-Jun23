1d477149c0745db28ebd80ebb9d0f3476f83a943
lparmenter <125324906+lparmenter@users.noreply.github.com>
2023-06-20 11:59:37 +1000

    Week 3 - UI
    Asses a few ui items

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,6 +10,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     int pickupCount;
     private Timer timer;
+    private bool gameOver = false;
+
+    [Header("UI")]
+    public GameObject inGamePanel;
+    public GameObject winPanel;
+    public TMP_Text scoreText;
+    public TMP_Text timerText;
+    public TMP_Text winTimeText;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +30,25 @@ public class PlayerController : MonoBehaviour
         //Get the timer object
         timer = FindObjectOfType<Timer>();
         timer.StartTimer();
+        //Turn on our in game panel
+        inGamePanel.SetActive(true);
+        //Turn on our in game panel
+        winPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        timerText.text = "Time" + timer.GetTime().ToString("F1");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (gameOver == true)
+        {
+            return;
+        }
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
@@ -57,14 +80,43 @@ public class PlayerController : MonoBehaviour
 
     void CheckPickups()
     {
-        //Print the amount of pick up
-        print("Pickups Left: " + pickupCount);
         
+        //Display the amount of pickups left in our scene
+        scoreText.text = "Pickups Left: " + pickupCount;
         if (pickupCount == 0)
         {
-            timer.StopTimer();
-            print("Congrats Mate, Your Time was: " + timer.GetTime());
+            WinGame();
+           
         }
+    }
+
+    void WinGame()
+    {
+        //Set the game over to true
+        gameOver = true;
+        //Stop timer
+        timer.StopTimer();
+        //Turn on our win panel
+        winPanel.SetActive(true);
+        //Tuen off our in game panel
+        inGamePanel.SetActive(false);
+        //Display the timer on the win time text
+        winTimeText.text = "Congrats Mate, Your Time was: " + timer.GetTime().ToString("F1");
+
+        //Set the velocity of the ridgedbody to zero
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+
+    //Temporary - Remove when doing mdules in A2
+    public void RestartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
 }
